68ecb3c1446167408fbab3f921b65e03fd4d94dc
MFSAMLA <s92065136@ousl.lk>
2024-12-02 16:58:23 +0530

    login page updated

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import InputType from '../../components/shared/forms/InputType';
+import Form from '../../components/shared/forms/Form';
+
 
 const Login = () => {
   return (
     <>
-      <div className='row'>
+      <div className='row g-0'>
         <div className='col-md-8 form-banner' >
             <img src='./assets/images/banner1.png' alt='loginImage'/>
         </div>
         <div className='col-md-4 form-container'>
-       <form>
-  <InputType labelText={'EMail'} labelFor={'forEMail'} inputType={'email'} name={'email'}/>
- 
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-
+       <Form formTitle={'Login Page'} submitBtn={'Login'} formType={'login'}/>
 
         </div>
 
