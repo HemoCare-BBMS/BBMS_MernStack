ef00b3c3be691546cf19621c9da49b3b05069cad
MFSAMLA <s92065136@ousl.lk>
2024-12-02 16:57:26 +0530

    register page updated

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Form from '../../components/shared/forms/Form'
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className='row g-0'>
+      <div className='col-md-8 form-banner' >
+        <img src ='./assets/images/banner1.png' alt='registerImage'/>
+      </div>
+      <div className='col-md-4 form-container'>
+    <Form formTitle={'Register'} submitBtn={'Register'} formType={'register'}/>
+      </div>
+
     </div>
   )
 }
 
-export default Register
+export default Register;
