f2d47d7aed752e1769bfa8ef8811c98e134308f7
MFSAMLA <s92065136@ousl.lk>
2024-12-08 20:55:20 +0530

    updated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,10 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSlice from './features/auth/authslice'
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './features/auth/authslice';
 
 const store = configureStore({
     reducer:{
-        auth:authSlice.reducer,
+        auth :authSlice.reducer,
     },
-})
+});
 
 export default store;
